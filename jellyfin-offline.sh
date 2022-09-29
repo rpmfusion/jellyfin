9cc0ef8cbc5b05e1ad5c475fdf4992b270bb6391
Michael Cronenworth <mike@cchtml.com>
2022-09-28 22:23:25 -0500

    ARM64 requires nupkg matching host dotnet version

--- jellyfin-offline.sh
+++ jellyfin-offline.sh
@@ -2,6 +2,7 @@
 
 JELLYFIN_VERSION=10.8.5
 DOTNET_VERSION=6.0.9
+DOTNET_HOST_VERSION=6.0.8
 
 # Retrieve neccessary .NET NuGet packages for offline building
 tar xf jellyfin-${JELLYFIN_VERSION}.tar.gz
@@ -13,7 +14,7 @@ pushd jellyfin-nupkgs2
 curl -L https://www.nuget.org/api/v2/package/runtime.any.System.Globalization/4.3.0 > runtime.any.system.globalization.4.3.0.nupkg
 curl -L https://www.nuget.org/api/v2/package/runtime.any.System.Runtime/4.3.0 > runtime.any.system.runtime.4.3.0.nupkg
 curl -L https://www.nuget.org/api/v2/package/Microsoft.NETCore.App.Runtime.linux-x64/${DOTNET_VERSION} >  microsoft.netcore.app.runtime.linux-x64.${DOTNET_VERSION}.nupkg
-curl -L https://www.nuget.org/api/v2/package/Microsoft.NETCore.App.Host.linux-x64/${DOTNET_VERSION} >  microsoft.netcore.app.host.linux-x64.${DOTNET_VERSION}.nupkg
+curl -L https://www.nuget.org/api/v2/package/Microsoft.NETCore.App.Host.linux-x64/${DOTNET_HOST_VERSION} >  microsoft.netcore.app.host.linux-x64.${DOTNET_HOST_VERSION}.nupkg
 curl -L https://www.nuget.org/api/v2/package/Microsoft.AspNetCore.App.Runtime.linux-x64/${DOTNET_VERSION} > microsoft.aspnetcore.app.runtime.linux-x64.${DOTNET_VERSION}.nupkg
 curl -L https://www.nuget.org/api/v2/package/System.Private.Uri/4.3.0 > system.private.uri.4.3.0.nupkg
 curl -L https://www.nuget.org/api/v2/package/runtime.unix.System.Private.Uri/4.3.0 > runtime.unix.system.private.uri.4.3.0.nupkg
